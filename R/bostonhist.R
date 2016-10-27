781ef24e493212d66a51a5c78b5386f6941ac668
Hari Pulugurta <charanp@gmail.com>
2016-10-27 13:19:39 -0400

    Test

--- R/bostonhist.R
+++ R/bostonhist.R
@@ -1,3 +1,13 @@
 bostonhist <- function(variable){
-	hist(Boston[[variable]], xlab=variable, main="Example plot with lazyload data")
+	#hist(Boston[[variable]], xlab=variable, main="Example plot with lazyload data")
+
+	x <- c(1:5); y <- x # create some data 
+	par(pch=22, col="red") # plotting symbol and color 
+	par(mfrow=c(2,4)) # all plots on one page 
+	opts = c("p","l","o","b","c","s","S","h") 
+	for(i in 1:length(opts)){ 
+	heading = paste("type=",opts[i]) 
+	plot(x, y, type="n", main=heading) 
+	lines(x, y, type=opts[i]) 
+	}
 }
